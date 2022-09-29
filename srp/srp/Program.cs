590599053aa116a4bd66b49455968f19c72ca4bc
brunoasika <82162157+brunoasika@users.noreply.github.com>
2022-09-29 14:57:28 +0100

    Add files via upload
    atm project

---
+++ srp/srp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srp
+{
+        interface Iuser
+    {
+        bool Login(string username, string password);
+        bool Register(string username, string password, string email);
+
+    }
+    
+    interface Iloggger
+    {
+        void LogError(string error);
+    }
+    interface Iemail
+    {
+        bool sendemail(string emailcontent);
+    }
+}
